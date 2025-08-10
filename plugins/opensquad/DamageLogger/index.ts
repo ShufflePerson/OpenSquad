902dcaf255cf1ba008a4cdba3197ee6b1aacd0fc
Shuffle <shuffleperson@pm.me>
2025-08-10 20:21:42 +0300

    Initial example plugin

---
+++ plugins/opensquad/DamageLogger/index.ts
@@ -0,0 +1,29 @@
+import { IPlugin } from "types/IPlugin.interface";
+import { LoggerService } from "services/Logger.service";
+import { EventManagerService } from "services/EventManager.service";
+import { IEventTakeDamage } from "types/services/LogParser/IParsedLog";
+
+
+export default class DamageLoggerPlugin implements IPlugin {
+    public readonly info!: IPlugin['info'];
+    public config: any;
+
+    constructor(
+        private readonly logger: LoggerService,
+        private readonly eventManager: EventManagerService
+    ) { }
+
+    public onEnable(): void {
+        this.eventManager.on('TAKE_DAMAGE', this.onTakeDamage);
+        this.logger.info(`[${this.info.name}] Now listening for damage events.`);
+    }
+
+    public onDisable(): void {
+        this.eventManager.off('TAKE_DAMAGE', this.onTakeDamage);
+        this.logger.info(`[${this.info.name}] Stopped listening for damage events.`);
+    }
+
+    private onTakeDamage = (log: IEventTakeDamage): void => {
+        console.log(log);
+    }
+}
